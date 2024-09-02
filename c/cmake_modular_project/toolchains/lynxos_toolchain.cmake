9be6d3a9dafa14fb8b1af2694e2f055314cb5859
brennen <brennen.j.sprimont@gmail.com>
2024-09-01 20:29:59 -0500

    Cleanup around c linked list and cmake modular project.

--- c/cmake_modular_project/toolchains/lynxos_toolchain.cmake
+++
@@ -1,6 +0,0 @@
-
-set(CMAKE_SYSTEM_NAME LynxOS)
-set(CMAKE_CROSSCOMPILING TRUE)
-set(CMAKE_SYSTEM_PROCESSOR powerpc)
-set(CMAKE_C_COMPILER /usr/los178/2.3.0/x86_dev/cdk/linux-coff-x86/usr/bin/gcc)
-set(CMAKE_CXX_COMPILER /usr/los178/2.3.0/x86_dev/cdk/linux-coff-x86/usr/bin/g++)
